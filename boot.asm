280bd925bf3513493e9d0a41f18480f7c6442be5
KernelOverseer <kerneloverseer@gmail.com>
2023-08-31 19:43:38 +0100

    stream 2, memory segmentation, interrupts, and reading from disk

--- boot.asm
+++ boot.asm
@@ -1,69 +1,57 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 
-start:	
-	mov si, message
-	call set_video_mode
-	mov cx, 100
-	mov dx, 100
-_loop:
-	call get_keystroke
-	cmp ah, 0x4B
-	je _left
-	cmp ah, 0x48
-	je _up
-	cmp ah, 0x4D
-	je _right
-	cmp ah, 0x50
-	je _down
-	jmp _loop
-_right:
-	inc cx
-	jmp _draw
-_left:
-	dec cx
-	jmp _draw
-_up:
-	dec dx
-	jmp _draw
-_down:
-	inc dx
-_draw:
-	call write_pixel
-	jmp _loop
+jmp 0x7c0:start ; jumping to segment 0x7c0 to set code segment
+
+start:
+	; setting the interrupt 0
+	mov word [0x0], divide_by_zero_handler ; set offset
+	mov word [0x2], 0x7c0	; set segment
+
+	cli ; clear interrupts
+	mov ax, 0x7c0
+	mov ds, ax ; setting the data segment
+	mov es, ax ; setting the extra segment
+	mov ax, 0x0
+	mov ss, ax ; setting the stack segment
+	mov sp, 0x7c00 ; setting the stack pointer
+	sti ; restore the interrupts
+
+	call read_from_disk ; read sector from disk
 	
-set_video_mode:
-	mov ah, 0x0
-	mov al, 0xd
-	int 0x10
+	mov si, 0x1000
+	call print ; printing the string loaded from sector 2
+
+	jmp $
+
+read_from_disk:
+	mov ah, 0x2
+	mov al, 1 ; read one sector
+	mov cl, 2 ; start from the second sector
+	mov bx, 0x1000 ; offset where the data will be loaded
+	int 0x13
 	ret
 
-write_pixel:
-	mov ah, 0xc
-	mov al, 255
-	int 0x10
-	ret
-
-get_keystroke:
-	mov ah, 0x0
-	int 0x16
-	ret
+divide_by_zero_handler:
+	mov si, error
+	call print
+	iret
 
 print_char:
-	mov ah, 0xE
-	int 0x10
-	ret
+  mov ah, 0xE
+  int 0x10
+  ret
 
 print:
-	mov al, [si]
-	cmp al, 0
-	je _end
-	call print_char
-	inc si
-	jmp print
-_end:
-	ret
+  mov al, [si]
+  call print_char
+  inc si
+  cmp al, 0
+  jne print
+  ret
 
-message: db "Hello Bootloader!", 0
-times 510 - ($ - $$) db 0
+message: db "JeremyCodes bootloader !", 0
+error: db "Error !", 0
+
+times 510-($ - $$) db 0
 dw 0xAA55
