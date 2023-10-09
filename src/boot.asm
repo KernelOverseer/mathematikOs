77b0e7e4bc98284295cbb5a83d800b0ea3da6ffa
KernelOverseer <kerneloverseer@gmail.com>
2023-10-09 14:20:51 +0100

    switched from using bios routines for loading the kernel into memory, to using ATA

--- src/boot.asm
+++ src/boot.asm
@@ -11,11 +11,7 @@ start:
 	mov es, ax     ; setting the extra segment
 	mov ss, ax     ; setting the stack segment
 	mov sp, 0x7c00 ; setting the stack pointer
-	sti            ; restore the interrupts
 
-	call read_from_disk
-
-	cli                   ; disable interrupts
 	; enabling A20 line
 	in   al,   0x92
 	or   al,   2
@@ -27,17 +23,15 @@ start:
 	or   al,   1
 	mov  cr0,  eax
 
-	jmp CODE_SEG:0x7E00
+	jmp CODE_SEG:load32
 
-read_from_disk:
-	mov ah, 0x2
-	mov al, 50    ; read 10 sectors
-	mov cl, 2     ; start from the second sector
-	mov bx, 0x7E00 ; offset where the data will be loaded
-	int 0x13
-	ret
-
-message: db "JeremyCodes bootloader !", 0
+[BITS 32]
+load32:
+    mov eax, 1
+    mov ecx, 100
+    mov edi, 0x0100000
+    call ata_lba_read
+    jmp CODE_SEG:0x0100000
 
 gdt_start: ; Start of our GDT
 gdt_null:  ; First Null GDT entry
@@ -67,7 +61,65 @@ gdt_end:
 gdt_descriptor:
     dw gdt_end - gdt_start-1
     dd gdt_start
-	
+
+ata_lba_read:
+    mov ebx, eax, ; Backup the LBA
+    ; Send the highest 8 bits of the lba to hard disk controller
+    shr eax, 24
+    or eax, 0xE0 ; Select the  master drive
+    mov dx, 0x1F6
+    out dx, al
+    ; Finished sending the highest 8 bits of the lba
+
+    ; Send the total sectors to read
+    mov eax, ecx
+    mov dx, 0x1F2
+    out dx, al
+    ; Finished sending the total sectors to read
+
+    ; Send more bits of the LBA
+    mov eax, ebx ; Restore the backup LBA
+    mov dx, 0x1F3
+    out dx, al
+    ; Finished sending more bits of the LBA
+
+    ; Send more bits of the LBA
+    mov dx, 0x1F4
+    mov eax, ebx ; Restore the backup LBA
+    shr eax, 8
+    out dx, al
+    ; Finished sending more bits of the LBA
+
+    ; Send upper 16 bits of the LBA
+    mov dx, 0x1F5
+    mov eax, ebx ; Restore the backup LBA
+    shr eax, 16
+    out dx, al
+    ; Finished sending upper 16 bits of the LBA
+
+    mov dx, 0x1f7
+    mov al, 0x20
+    out dx, al
+
+    ; Read all sectors into memory
+.next_sector:
+    push ecx
+
+; Checking if we need to read
+.try_again:
+    mov dx, 0x1f7
+    in al, dx
+    test al, 8
+    jz .try_again
+
+; We need to read 256 words at a time
+    mov ecx, 256
+    mov dx, 0x1F0
+    rep insw
+    pop ecx
+    loop .next_sector
+    ; End of reading sectors into memory
+    ret
 
 times 510-($ - $$) db 0
 dw 0xAA55
